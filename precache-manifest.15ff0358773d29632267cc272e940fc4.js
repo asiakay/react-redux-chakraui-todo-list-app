461a10c56bdea7859e673c0b493913915833c4a6
Microsoft Learn Student <learn@contoso.com>
2021-05-30 11:19:52 -0400

    Updates

---
+++ precache-manifest.15ff0358773d29632267cc272e940fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca19b4c50d07a3ca33cb439ea0a3a22",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "b25aea07047565e09d02",
+    "url": "/todo-list-app/static/css/main.4d8c9c87.chunk.css"
+  },
+  {
+    "revision": "90d269faf6bebf8f64f4",
+    "url": "/todo-list-app/static/js/2.ccff5cb7.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/todo-list-app/static/js/2.ccff5cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25aea07047565e09d02",
+    "url": "/todo-list-app/static/js/main.3d084b62.chunk.js"
+  },
+  {
+    "revision": "fc1ebefe855e12ff14a2",
+    "url": "/todo-list-app/static/js/runtime-main.d4521ec4.js"
+  }
+]);
